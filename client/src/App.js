5197f6d8df33a2a993c6ca2f198516431fb834ca
Ari Vidal <avidal@teampacesetter.com>
2019-05-16 16:32:50 -0400

    making minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Login1 from "./pages/Login1";
 import Checkout from './pages/ClientCheckout';
 import Report from './pages/Reporting';
@@ -186,8 +186,10 @@ class App extends Component {
         });
     };
     render() {
+
         return (
             <div>
+                <Redirect from="/" exact to="/login1" />
                 <Switch>
                     <Route exact path="/login1"><Login1 getValue={this.getValue.bind(this)}  usrname={this.state.usrname} open3={this.state.open3} open4={this.state.open4} open5={this.state.open5} handlesubmit={this.handlesubmit.bind(this)} handleAdd={this.handleAdd.bind(this)} open={this.state.open} open1={this.state.open1} handleClose={this.handleClose.bind(this)}/></Route>
                     <Route exacht path="/signup" component={SignUP}/>
