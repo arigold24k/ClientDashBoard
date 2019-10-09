077b9641f5a7e1fcf27dc63a465dfb6e9884b768
Ari Vidal <ariel.m.vidal@gmail.com>
2019-10-09 11:26:09 -0400

    making some heroku changes

--- server.js
+++ server.js
@@ -19,13 +19,23 @@ let http = require('http').Server(app);
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.use("/", express.static(path.join(__dirname, 'client/build')));
     // Handle React routing, return all requests to React app
     // app.get('/*', function(req, res) {
     //     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     // });
-    app.get('/*', (res, req) => {
-        res.sendFile(path.join(__dirname+'/client/build/index.html'));
+
+    app.use(helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: ["'self'"],
+            styleSrc: ["'self'"],
+            imgSrc: ["'self"]
+        }
+    }));
+
+    app.get("/*", (res, req) => {
+        //res.sendFile(path.join(__dirname+'/client/build/index.html'));
+        res.sendfile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
